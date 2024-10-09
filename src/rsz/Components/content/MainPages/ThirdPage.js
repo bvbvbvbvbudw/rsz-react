3cb31d81e3f16e5708c56a45072d04bdf89633dc
Bogdan Bikbulatov <batmansila1234@gmail.com>
2024-10-09 20:57:33 +0300

    update

--- src/rsz/Components/content/MainPages/ThirdPage.js
+++ src/rsz/Components/content/MainPages/ThirdPage.js
@@ -19,7 +19,14 @@ export default function ThirdPage()
                 <div className="container">
                     <img src={photo} alt="Photo"/>
                     <div className="info">
-
+                        <div className={styles.title}>Нам понад 125 років</div>
+                        <p className={styles.topDescription}>
+                            Завдяки багаторічному досвіду у галузі скловаріння, вигідному розташуванню кварцових піщаних кар'єрів та систематичному технічному вдосконаленню виробничого процесу, ПрАТ «Рокитнівський скляний завод», який працює з 1898 року, успішно входить у трійку лідерів виробників скляних пляшок в Україні.
+                        </p>
+                        <div className={styles.bottomWrapper}>
+                            <p className={styles.leftSide}>Покриваємо усі ризики</p>
+                            <p className={styles.bottomDescription}>Хоча рідко, але можливі непередбачені обставини, які можуть вплинути на доставку. Навіть у випадку таких ситуацій, ми готові вирішити всі непорозуміння, навіть в умовах виникнення надзвичайних обставин.</p>
+                        </div>
                     </div>
                 </div>
             </main>
