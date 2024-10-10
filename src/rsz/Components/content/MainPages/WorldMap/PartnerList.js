8e504a594984c93a602f27db1c17a0479d3be625
Bogdan Bikbulatov <batmansila1234@gmail.com>
2024-10-10 18:19:25 +0300

    changes

--- src/rsz/Components/content/MainPages/WorldMap/PartnerList.js
+++ src/rsz/Components/content/MainPages/WorldMap/PartnerList.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import styles from './PartnerList.module.css';
 
+import netherlands from '../../../../assets/img/netherlands.png';
+import sweden from '../../../../assets/img/Flag_of_Sweden 1.png';
+import lat from '../../../../assets/img/Flag_of_Latvia 1.png';
+import italy from '../../../../assets/img/Flag_of_Italy 1.png';
+import belgium from '../../../../assets/img/Flag_of_Belgium 1.png';
+import norway from '../../../../assets/img/Flag_of_Norway 1.png';
+import romania from '../../../../assets/img/Flag_of_Romania 1.png';
+import spain from '../../../../assets/img/image 9.png';
+import france from '../../../../assets/img/Flag_of_France_(1794–1815,_1830–1974) 1.png';
+import estonia from '../../../../assets/img/Flag_of_Estonia 1.png';
+import finland from '../../../../assets/img/Flag_of_Finland 1.png';
+import lithuania from '../../../../assets/img/Flag_of_Lithuania 1.png';
+
 const partners = [
-    { name: 'Голландія', flag: 'https://cdn.builder.io/api/v1/image/assets/TEMP/9e19ae2578ed0f84a38415cf7504dffc6ea3d03a2d421991fb03db01e8f06df2?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27' },
-    { name: 'Швеція', flag: 'https://cdn.builder.io/api/v1/image/assets/TEMP/2ad0b78704330067ae88967426f418106d781d2dc2373eb717d0099cebce9f18?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27' },
-    { name: 'Латвія', flag: 'https://cdn.builder.io/api/v1/image/assets/TEMP/599c3097c2d514630efd62a59222a141fb7a23f840d2341fde4a403a326ca13d?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27' },
-    { name: 'Італія', flag: 'https://cdn.builder.io/api/v1/image/assets/TEMP/240b37cc92dd2e72e890d502dd27fb7c36e7e8a12aa9e4d2b3a23858b1a8cf05?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27' },
-    { name: 'Бельгія', flag: 'https://cdn.builder.io/api/v1/image/assets/TEMP/03f8a94165660746fff150b79827228c2d24b8a00b009a96bbf445b8d14ba3a6?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27' },
-    { name: 'Норвегія', flag: 'https://cdn.builder.io/api/v1/image/assets/TEMP/c28e65897ea4ba886dfdb301179f69693c4d58ba17f98c028caa7f4896521267?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27' },
-    { name: 'Румунія', flag: 'https://cdn.builder.io/api/v1/image/assets/TEMP/4ff23cda459976ebdf8be839750a307a8e2f543e20c71e7f60ae0fb413068975?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27' },
-    { name: 'Іспанія', flag: 'https://cdn.builder.io/api/v1/image/assets/TEMP/edea4970f56ad5edfc2209a301254bdb92e2c83ce32fc7af532b78c197c1d674?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27' },
-    { name: 'Франція', flag: 'https://cdn.builder.io/api/v1/image/assets/TEMP/90633161acc9b41de410dbe82b12cdff7b25e93a93c6d3f9333bf58e319c0566?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27' },
-    { name: 'Естонія', flag: 'https://cdn.builder.io/api/v1/image/assets/TEMP/2741ba0388e9e15fd99a924a8677c0ccf18610da2a454650096adc612b5d16da?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27' },
-    { name: 'Фінляндія', flag: 'https://cdn.builder.io/api/v1/image/assets/TEMP/c4f6019fdc01748102fb69d2950c12d600fa749cb56c4860690a7eea41ee5906?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27' },
-    { name: 'Литва', flag: 'https://cdn.builder.io/api/v1/image/assets/TEMP/c4e08b4dd23539a30a9c485a40b4b7dbed7130231dd26fb40b4c93d694c1d387?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27' },
+    { name: 'Голландія', flag: netherlands },
+    { name: 'Швеція', flag: sweden },
+    { name: 'Латвія', flag: lat },
+    { name: 'Італія', flag: italy },
+    { name: 'Бельгія', flag: belgium },
+    { name: 'Норвегія', flag: norway },
+    { name: 'Румунія', flag: romania },
+    { name: 'Іспанія', flag: spain },
+    { name: 'Франція', flag: france },
+    { name: 'Естонія', flag: estonia },
+    { name: 'Фінляндія', flag: finland },
+    { name: 'Литва', flag: lithuania },
 ];
 
 const PartnerList = () => {
--- src/rsz/Components/content/ProductionWineComponent.js
+++ src/rsz/Components/content/ProductionWineComponent.js
@@ -3,6 +3,9 @@ import Menu from "./Menu";
 import styles from "../../assets/css/ProductionWineComponent.module.css";
 
 import wineOne from '../../assets/img/wine1.png';
+import chimics from '../../assets/img/Group 168.png';
+import piwo from '../../assets/img/Group 166.png';
+import mineral from '../../assets/img/ПродукціяВода.png';
 
 const menuItems = [
     { title: "Тара для пива", image: "https://cdn.builder.io/api/v1/image/assets/TEMP/8d82b446c2e9635b8255d1d82653a9bf444a92f671c97e4b7d775f84c16792b4?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27" },
@@ -12,10 +15,10 @@ const menuItems = [
 ];
 
 const backgroundImages = [
+    piwo,
     wineOne,
-    "https://cdn.builder.io/api/v1/image/assets/TEMP/8bd1762ac14c9cfc2fbd7da91e8e64c5a52b7e44000b3fe7a15ffecf8f826261?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27",
-    "https://cdn.builder.io/api/v1/image/assets/TEMP/1669f0bc20e60d784579b0ffd8a651f51862c8aedcad771d85433d2250860763?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27",
-    "https://cdn.builder.io/api/v1/image/assets/TEMP/50db57235891b84736c3cb47a8644cf8297234c7efec3fd3d27ed737b62e5c0a?placeholderIfAbsent=true&apiKey=4de2176252d24e8ebcd3e8166c772c27"
+    mineral,
+    chimics
 ];
 
 const ProductionWineComponent = () => {
